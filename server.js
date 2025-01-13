a83e79c3aef170241490fdb8715662e679add6fe
Singh Pritesh <priteshkumarsingh@fxis.ai>
2025-01-13 12:15:28 +0530

    integrate mysql

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express")
-const cors = require("cors")
-const mysql = require("mysql")
+const cors = require("cors");
+const mySqlPool = require("./db/db");
 
 
 const app = express();
 app.use(cors())
 const PORT = process.env.PORT || 5001;
 
+mySqlPool.query("SELECT 1").then(() => {
+
+    //db connection
+    console.log("Database connected successfully");
+    
+    app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+}).catch((error) => {console.log("DB Connection Fail")
+
+});
 
 
-
-
-
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
