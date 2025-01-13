f733cd52a84259139aeb6fbea7957d620541c867
Singh Pritesh <singhpritesh7599@gmail.com>
2025-01-13 10:43:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const cors = require("cors")
+const mysql = require("mysql")
+
+
+const app = express();
+app.use(cors())
+const PORT = 5001;
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
